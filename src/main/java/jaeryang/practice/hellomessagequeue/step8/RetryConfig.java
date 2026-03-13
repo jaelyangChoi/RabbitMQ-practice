7dc4a97920f124096dbb277c939bfc6d271e92e8
Jealyang <jaelyangchoi@gmail.com>
2026-03-13 23:30:35 +0900

    feat: xml 설정ìetry 설정하기

--- src/main/java/jaeryang/practice/hellomessagequeue/step8/RetryConfig.java
+++ src/main/java/jaeryang/practice/hellomessagequeue/step8/RetryConfig.java
@@ -1,12 +1,11 @@
 package jaeryang.practice.hellomessagequeue.step8;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
-@Configuration
+// @Configuration
 public class RetryConfig {
 
 	@Bean
